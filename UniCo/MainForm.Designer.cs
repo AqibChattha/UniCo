fbf8765a2db5c75c72942e47e37aa6ec00c353aa
Aqib Chattha <aqibali543@gmail.com>
2022-03-10 17:58:46 +0500

    Desktop App Creation in C#
    Creating a Windows Form App (.NET Framework) application with visual studio in C#. There is also some slight changes in the MainForm and layout of the form is drawn. It will have a side menu that will be used to transition between different user controls.

---
+++ UniCo/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace UniCo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlSideMenu = new System.Windows.Forms.Panel();
+            this.pnlContainer = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlMain.SuspendLayout();
+            this.pnlSideMenu.SuspendLayout();
+            this.pnlContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.White;
+            this.pnlMain.Controls.Add(this.pnlContainer);
+            this.pnlMain.Controls.Add(this.pnlSideMenu);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(800, 450);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // pnlSideMenu
+            // 
+            this.pnlSideMenu.AutoScroll = true;
+            this.pnlSideMenu.BackColor = System.Drawing.Color.White;
+            this.pnlSideMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSideMenu.Controls.Add(this.label1);
+            this.pnlSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideMenu.Name = "pnlSideMenu";
+            this.pnlSideMenu.Size = new System.Drawing.Size(146, 450);
+            this.pnlSideMenu.TabIndex = 1;
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.BackColor = System.Drawing.Color.White;
+            this.pnlContainer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlContainer.Controls.Add(this.label2);
+            this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContainer.Location = new System.Drawing.Point(146, 0);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Size = new System.Drawing.Size(654, 450);
+            this.pnlContainer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("NSimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(43, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UniCo";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("NSimSun", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(230, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Welcome to UniCo";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "MainForm";
+            this.Text = "UniCo";
+            this.pnlMain.ResumeLayout(false);
+            this.pnlSideMenu.ResumeLayout(false);
+            this.pnlSideMenu.PerformLayout();
+            this.pnlContainer.ResumeLayout(false);
+            this.pnlContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnlContainer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlSideMenu;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
